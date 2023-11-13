787c5578157e326a1446b2eced968388d8862ae2
DSUN <dsun@hanmail.net>
2023-11-13 17:07:30 +0900

    초기 커밋.

---
+++ README.md
@@ -0,0 +1,10 @@
+# MetaFrm.Service.RabbitMQProducer
+
+### .Net 6.0
+[![build 6.0](https://github.com/MetaFrm/MetaFrm.Service.RabbitMQProducer/actions/workflows/build_6.0.yml/badge.svg)](https://github.com/MetaFrm/MetaFrm.Service.RabbitMQProducer/actions/workflows/build_6.0.yml)
+[![NuGet version (MetaFrm.Service.RabbitMQProducer.net6.0)](https://img.shields.io/nuget/v/MetaFrm.Service.RabbitMQProducer.net6.0)](https://www.nuget.org/packages/MetaFrm.Service.RabbitMQProducer.net6.0/)
+[![NuGet downloads (MetaFrm.Service.RabbitMQProducer.net6.0)](https://img.shields.io/nuget/dt/MetaFrm.Service.RabbitMQProducer.net6.0)](https://www.nuget.org/packages/MetaFrm.Service.RabbitMQProducer.net6.0/)
+### .Net 7.0
+[![build 7.0](https://github.com/MetaFrm/MetaFrm.Service.RabbitMQProducer/actions/workflows/build_7.0.yml/badge.svg)](https://github.com/MetaFrm/MetaFrm.Service.RabbitMQProducer/actions/workflows/build_7.0.yml)
+[![NuGet version (MetaFrm.Service.RabbitMQProducer.net7.0)](https://img.shields.io/nuget/v/MetaFrm.Service.RabbitMQProducer.net7.0)](https://www.nuget.org/packages/MetaFrm.Service.RabbitMQProducer.net7.0/)
+[![NuGet downloads (MetaFrm.Service.RabbitMQProducer.net7.0)](https://img.shields.io/nuget/dt/MetaFrm.Service.RabbitMQProducer.net7.0)](https://www.nuget.org/packages/MetaFrm.Service.RabbitMQProducer.net7.0/)
---
+++ src/RabbitMQProducer.cs
@@ -0,0 +1,83 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace MetaFrm.Service
+{
+    /// <summary>
+    /// RabbitMQProducer
+    /// </summary>
+    public class RabbitMQProducer : IServiceString, IDisposable
+    {
+        private string ConnectionString { get; set; }
+        private string QueueName { get; set; }
+
+        private IConnection? _connection;
+        private IModel? _model;
+
+        /// <summary>
+        /// RabbitMQProducer
+        /// </summary>
+        public RabbitMQProducer(string connectionString, string queueName)
+        {
+            this.ConnectionString = connectionString;
+            this.QueueName = queueName;
+
+            this.Init();
+        }
+
+        private void Init()
+        {
+            this.Close();
+
+            this._connection = new ConnectionFactory
+            {
+                Uri = new(this.ConnectionString)
+            }.CreateConnection();
+
+            this._model = _connection.CreateModel();
+            this._model.QueueDeclare(queue: this.QueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+        }
+        private void Close()
+        {
+            if (_model != null && _model.IsOpen)
+            {
+                _model.Close();
+                _model = null;
+            }
+            if (_connection != null && _connection.IsOpen)
+            {
+                _connection.Close();
+                _connection = null;
+            }
+        }
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                this.Close();
+        }
+
+        //https://dotnetblog.asphostportal.com/how-to-make-sure-your-asp-net-core-keep-running-on-iis/
+        string IServiceString.Request(string data)
+        {
+            if (_model == null)
+                return "";
+
+            _model.BasicPublish(string.Empty, this.QueueName, null, Encoding.UTF8.GetBytes(data));
+
+            return "";
+        }
+    }
+}
